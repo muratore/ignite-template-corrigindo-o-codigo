7af539b072a13552cf684175f672f89e0a830212
Alessandro Muratore <muratore@gmail.com>
2022-03-22 19:09:00 -0300

    Challenge done

--- src/index.js
+++ src/index.js
@@ -22,24 +22,28 @@ app.post("/repositories", (request, response) => {
     techs,
     likes: 0
   };
-
-  return response.json(repository);
+  repositories.push(repository);
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
+  const {title, url, techs } = request.body;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
-
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
+  
+  repositories[repositoryIndex].title = title;
+  repositories[repositoryIndex].url = url;
+  repositories[repositoryIndex].techs = techs;
+  updatedRepository.likes = 0;
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
-  repositories[repositoryIndex] = repository;
-
+  
   return response.json(repository);
 });
 
@@ -48,7 +52,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -60,15 +64,16 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+ const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
+  const repository = repositories[repositoryIndex];
 
-  const likes = ++repositories[repositoryIndex].likes;
+  repository.likes ++;
 
-  return response.json('likes');
+  return response.json(repository);
 });
 
 module.exports = app;
